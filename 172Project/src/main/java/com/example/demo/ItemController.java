61ec8ecd0766eff5149f66b183d940e8826dee9c
richardphamsjsu2016 <42747446+richardphamsjsu2016@users.noreply.github.com>
2020-05-12 17:59:36 -0700

    Update ItemController.java

--- 172Project/src/main/java/com/example/demo/ItemController.java
+++ 172Project/src/main/java/com/example/demo/ItemController.java
@@ -38,6 +38,7 @@ import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.example.demo.model.Item;
 
+//Created by Christina Nguyen
 @RestController
 public class ItemController {
 
